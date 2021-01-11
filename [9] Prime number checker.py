a81e3c31a5b47712a15fce266b19ae7ad57abd6e
Vaidik V <63895478+VaidikV@users.noreply.github.com>
2021-01-11 10:07:17 +0530

    Add files via upload

---
+++ [9] Prime number checker.py
@@ -0,0 +1,16 @@
+
+def prime_checker(number):
+    is_prime = True
+    if number > 1:
+        for i in range(2,number//2):
+            if number % i == 0:
+                is_prime = False
+        if is_prime == True:
+            print("The number is a prime number.")
+        else:
+            print("The number is not a prime number.")
+    else:
+        print("number is not a prime number")
+print("Welcome to Python prime number checker!")
+n = int(input("Check this number: "))
+prime_checker(number=n)
