1eac583de4212af2940a53a0263cd0287a5e38e9
Vaidik V <63895478+VaidikV@users.noreply.github.com>
2021-01-20 14:08:50 +0530

    Add files via upload

---
+++ [11] Calculator.py
@@ -0,0 +1,61 @@
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+  
+properties = { "+": add,
+"-": subtract,
+"*": multiply,
+"/": divide,
+}
+
+
+
+def calculator():
+  print("""
+ _____________________
+|  _________________  |
+| |              0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+""")
+  num1 = float(input("What's the first number?: "))
+  for symbols in properties:
+    print(symbols)
+
+  continue_using = True
+
+  while continue_using:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+
+    calculation_function = properties[operation_symbol]
+    answer = calculation_function(num1, num2) 
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    continue_using = input(f"Type 'y' to continue calculating using {answer} or type 'n' to start again. ").lower()
+
+    if continue_using == "n":
+      calculator()
+    else:
+      num1 = answer
+
+calculator() 
