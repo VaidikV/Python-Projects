849203027807c335d18a2faf48900bed24c5a1b3
Vaidik V <63895478+VaidikV@users.noreply.github.com>
2021-03-08 04:08:03 +0530

    Update [9] Prime number checker.py
    Refined the code

--- [9] Prime number checker.py
+++ [9] Prime number checker.py
@@ -2,15 +2,17 @@
 def prime_checker(number):
     is_prime = True
     if number > 1:
-        for i in range(2,number//2):
+        for i in range(2, number):
             if number % i == 0:
                 is_prime = False
-        if is_prime == True:
+        if is_prime:
             print("The number is a prime number.")
         else:
             print("The number is not a prime number.")
     else:
         print("number is not a prime number")
+        
 print("Welcome to Python prime number checker!")
+
 n = int(input("Check this number: "))
 prime_checker(number=n)
